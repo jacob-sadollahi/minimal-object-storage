2d1eb4530031c3348ca1ed1c19ce6b2ee5739b50
salvador <mehdi73ee@gmail.com>
2020-12-17 00:29:56 +0330

    added env for database connection

--- src/core/conf.py
+++ src/core/conf.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from pymongo import MongoClient
 from urllib.parse import quote_plus
@@ -9,12 +10,14 @@ app = Flask(__name__)
 swag = Swagger(app)
 db = MongoEngine()
 
-app.config['JWT_SECRET_KEY'] = 'ChangeMe!@#$'
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'ChangeMe!@#$')
 jwt = JWTManager(app)
-db_username = "mongo_user"
-db_name = "mymongo"
-db_password = quote_plus("mongoXYZ123!@#")
-db_host = "localhost"
+
+DEBUG = os.getenv("DEBUG", True)
+db_name = os.getenv("DB_NAME", "mymongo")
+db_username = os.getenv("MONGO_INITDB_ROOT_USERNAME", "mongo_user")
+db_password = quote_plus(os.getenv("MONGO_INITDB_ROOT_PASSWORD", "mongoXYZ123!@#"))
+db_host = os.getenv("DB_HOST", "mongo-db")
 db_port = 27017
 db_authSource = "admin"
 mongo_uri = f'mongodb://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}?authSource={db_authSource}'
