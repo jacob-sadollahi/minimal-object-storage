f1f5ba4f56212f8220a4a40db152653356183255
salvador <mehdi73ee@gmail.com>
2020-12-17 00:30:19 +0330

    added prefix for middleware api

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-from core.conf import app
+from core.conf import app, DEBUG
 
 from apps.storage.views import CheckBucketRules
 from apps.user.views import Auth, Register
+common_prefix = "/middleware/api/v1"
+app.add_url_rule(f'{common_prefix}/user/auth', view_func=Auth.as_view('auth'), methods=['POST'])
+app.add_url_rule(f'{common_prefix}/user/register', view_func=Register.as_view('register'), methods=['POST'])
 
-app.add_url_rule('/api/user/auth', view_func=Auth.as_view('auth'), methods=['POST'])
-app.add_url_rule('/api/user/register', view_func=Register.as_view('register'), methods=['POST'])
-
-app.add_url_rule('/api/storage/bucket/check/<bucket_name>', view_func=CheckBucketRules.as_view('bucket-check'), methods=['GET'])
+app.add_url_rule(f'{common_prefix}/storage/bucket/check/<bucket_name>', view_func=CheckBucketRules.as_view('bucket-check'), methods=['GET'])
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=DEBUG)
